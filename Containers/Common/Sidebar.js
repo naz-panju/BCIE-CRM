11f266f6b42ed591ec5f09e5212bc92229d991ee
muzammil@spiderworks.in <muzammil@spiderworks.in>
2024-02-26 20:52:02 +0530

    tailwind

--- Containers/Common/Sidebar.js
+++ Containers/Common/Sidebar.js
@@ -2,15 +2,6 @@ import React from 'react'
 
 function Sidebar() {
 
-    const sidebarStyle = {
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        height: '100%', // Adjust the height as needed
-        width: '200px', // Adjust the width as needed
-        backgroundColor: '#f0f0f0', // Example background color
-        // Add any other styles you want for your sidebar
-    };
 
     return (
        <div className='container'>
