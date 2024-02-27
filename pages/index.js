4a5d47a05a02919e10d3a29e4daabfb74483b187
muzammil@spiderworks.in <muzammil@spiderworks.in>
2024-02-27 16:05:25 +0530

    removed serverprops

--- pages/index.js
+++ pages/index.js
@@ -39,19 +39,19 @@ export default function Home() {
 }
 
 
-export async function getServerSideProps(context) {
-  const session = await getSession(context)
+// export async function getServerSideProps(context) {
+//   const session = await getSession(context)
 
 
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/login',
-        permanent: false,
-      },
-    };
-  }
-  return {
-    props: { session },
-  };
-}
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: '/login',
+//         permanent: false,
+//       },
+//     };
+//   }
+//   return {
+//     props: { session },
+//   };
+// }
