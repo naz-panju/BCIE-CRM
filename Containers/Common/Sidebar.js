e22581be0144894792531adb1f35acfc5d4bb371
ArunSpiderWorks <arun@spiderworks.in>
2024-02-27 09:45:40 +0530

    latest changes

--- Containers/Common/Sidebar.js
+++ Containers/Common/Sidebar.js
@@ -1,27 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function Sidebar() {
+import Image from "next/image";
+import LogoIcon from '@/styles/meritto-icon.svg';
+import Logo from '@/styles/merittologo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faTableColumns } from '@fortawesome/free-solid-svg-icons';
+import { faHouseChimney } from '@fortawesome/free-solid-svg-icons';
 
-    const sidebarStyle = {
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        height: '100%', // Adjust the height as needed
-        width: '200px', // Adjust the width as needed
-        backgroundColor: '#f0f0f0', // Example background color
-        // Add any other styles you want for your sidebar
+const Sidebar = () => {
+    const [isExpanded, setIsExpanded] = useState(false);
+  
+    const handleMouseEnter = () => {
+      setIsExpanded(true);
     };
-
+  
+    const handleMouseLeave = () => {
+      setIsExpanded(false);
+    };
+  
+      
     return (
-       <div className='container'>
-            <div className='sidebar'>
-                <div>
-                    Menus
-                </div>
-    
-            </div>
-       </div>
+        
+
+
+
+<nav id="sidebar" className={`sidebar sidebar-mask active ${isExpanded ? 'active' : ''}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+    <div className="sidebar__inner scrollCustom">
+        <div className="site-logo onlyDesktop">
+            <a href="#" className="inbl valigntop logo">
+                <Image src={LogoIcon} alt='LogoIcon' width={30} className='tiny-sidebar' />
+                <Image src={Logo} alt='LogoIcon' width={145} className='max-sidebar' />
+            </a>
+        </div>
+
+        <ul className="list-unstyled components mainList" id="menuList">
+
+            <li><a href='#'><i><FontAwesomeIcon icon={faMagnifyingGlass} /></i><span>Menu1</span></a></li>
+            <li><a href='#'><i><FontAwesomeIcon icon={faTableColumns} /></i><span>Menu2</span></a></li>
+            <li><a href='#'><i><FontAwesomeIcon icon={faHouseChimney} /></i><span>Menu2</span></a></li>
+            <li><a href='#'><i><FontAwesomeIcon icon={faMagnifyingGlass} /></i><span>Menu1</span></a></li>
+            <li><a href='#'><i><FontAwesomeIcon icon={faTableColumns} /></i><span>Menu2</span></a></li>
+            <li><a href='#'><i><FontAwesomeIcon icon={faHouseChimney} /></i><span>Menu2</span></a></li>
+            <li><a href='#'><i><FontAwesomeIcon icon={faMagnifyingGlass} /></i><span>Menu1</span></a></li>
+            <li><a href='#'><i><FontAwesomeIcon icon={faTableColumns} /></i><span>Menu2</span></a></li>
+            <li><a href='#'><i><FontAwesomeIcon icon={faHouseChimney} /></i><span>Menu2</span></a></li>
+            <li><a href='#'><i><FontAwesomeIcon icon={faMagnifyingGlass} /></i><span>Menu1</span></a></li>
+            <li><a href='#'><i><FontAwesomeIcon icon={faTableColumns} /></i><span>Menu2</span></a></li>
+            <li><a href='#'><i><FontAwesomeIcon icon={faHouseChimney} /></i><span>Menu2</span></a></li>
+            <li><a href='#'><i><FontAwesomeIcon icon={faMagnifyingGlass} /></i><span>Menu1</span></a></li>
+            <li><a href='#'><i><FontAwesomeIcon icon={faTableColumns} /></i><span>Menu2</span></a></li>
+            <li><a href='#'><i><FontAwesomeIcon icon={faHouseChimney} /></i><span>Menu2</span></a></li>
+        </ul>
+    </div>
+</nav>
+
+
     )
 }
 
-export default Sidebar
+export default Sidebar;
