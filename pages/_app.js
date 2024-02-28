eebdfa73f31764219935c0a2f916b9f2f97ec4cb
muzammil@spiderworks.in <muzammil@spiderworks.in>
2024-02-28 11:04:50 +0530

    https to http

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,9 @@ import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }) {
+
+  console.log(pageProps);
+
   return <SessionProvider>
     <Toaster />
     <Component {...pageProps} />
