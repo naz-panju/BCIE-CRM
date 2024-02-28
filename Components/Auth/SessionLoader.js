456ee80d9f392d5b7fbf3fe304138954d268edbb
muzammil@spiderworks.in <muzammil@spiderworks.in>
2024-02-28 13:22:08 +0530

    local storage

--- Components/Auth/SessionLoader.js
+++ Components/Auth/SessionLoader.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {useRouter} from "next/router";
-import {signIn, useSession} from "next-auth/react";
-import {setToken} from "@/data/config/http";
+import { useRouter } from "next/router";
+import { signIn, useSession } from "next-auth/react";
+import { setToken } from "@/data/config/http";
 
-const SessionLoader = ({children}) => {
+const SessionLoader = ({ children }) => {
     const router = useRouter()
     const session = useSession()
 
@@ -13,16 +13,21 @@ const SessionLoader = ({children}) => {
         return <div className="loading" />
     }
 
-    if (session.status === 'authenticated') {
-        setToken(session.data.user.token)
-        localStorage.setItem('token',session.data.user.token)
+    // if (session.status === 'authenticated') {
+    //     setToken(session.data.user.token)
+    //     localStorage.setItem('token',session.data.user.token)
+    // }
+    if (typeof window !== 'undefined' && session.status === 'authenticated' && session.data?.user?.token) {
+        setToken(session.data.user.token);
+        localStorage.setItem('token', session.data.user.token);
     }
+
 
     if (router.pathname === '/login' && session.status === 'authenticated') {
         router.push('/')
     }
 
-    if (session.status === "unauthenticated"){
+    if (session.status === "unauthenticated") {
         signIn();
     }
 
