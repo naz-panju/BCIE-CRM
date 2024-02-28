708b2b3b2b840437a9c52582ef9d438d0f298ff5
muzammil@spiderworks.in <muzammil@spiderworks.in>
2024-02-28 12:42:26 +0530

    app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,11 @@ import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
 
-export default function App({ Component, pageProps }) {
-  return <SessionProvider>
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
+
+  console.log(pageProps);
+
+  return <SessionProvider session={session}>
     <Toaster />
     <Component {...pageProps} />
   </SessionProvider>
