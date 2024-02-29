9de94f700a044f2dca46ada320cbf446936c67bb
muzammil@spiderworks.in <muzammil@spiderworks.in>
2024-02-29 10:32:06 +0530

    header chnage

--- Components/Header.js
+++ Components/Header.js
@@ -2,8 +2,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { faGear } from '@fortawesome/free-solid-svg-icons';
-import { SearchOffOutlined, SearchOutlined } from '@mui/icons-material';
-// import { Tooltip } from '@mui/material';
+import { Tooltip } from '@mui/material';
 
 
 
@@ -14,47 +13,47 @@ const Header = ({ }) => {
       <div className='container-fluid'>
         <div className='row flex items-center'>
           <div className="w-full md:w-6/12 lg:w-6/12 pad-15 clg_header clg_header_">
-            <button type="button" className="inbl bgnone bdrnone pdnone valigntop sbarCollapsebtn sidebarCollapse"><span></span><span></span><span></span></button>
+            <button type="button" class="inbl bgnone bdrnone pdnone valigntop sbarCollapsebtn sidebarCollapse"><span></span><span></span><span></span></button>
           </div>
 
           <div className='w-full md:w-6/12 lg:w-6/12 pad-15 '>
             <div className='navbar-right'>
               <ul>
-                {/* <li>
-                  <div className="tooltip-container">
-                    <div className="tooltip">
-                      <Tooltip title={'test'}><span className="tooltiptext">Tooltip</span></Tooltip>
-                    </div>
-                  </div>
-                </li> */}
                 <li>
                   <div className="tooltip-container">
-                    <div className="tooltip"><i><FontAwesomeIcon icon={faGear} /></i>
-                      <span className="tooltiptext">Service</span>
+                    <div class="tooltip">
+                      <Tooltip title={'test'}><span class="tooltiptext">Tooltip</span></Tooltip>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className="tooltip-container">
+                    <div class="tooltip"><i><FontAwesomeIcon icon={faGear} /></i>
+                      <span class="tooltiptext">Service</span>
                     </div>
                   </div>
                 </li>
 
                 <li>
                   <div className="tooltip-container">
-                    <div className="tooltip"><i><SearchOutlined fontSize='small' /></i>
-                      <span className="tooltiptext">Service</span>
+                    <div class="tooltip"><i><FontAwesomeIcon icon={faMagnifyingGlass} /></i>
+                      <span class="tooltiptext">Service</span>
                     </div>
                   </div>
                 </li>
 
                 <li>
                   <div className="tooltip-container">
-                    <div className="tooltip"><i><FontAwesomeIcon icon={faMagnifyingGlass} /></i>
-                      <span className="tooltiptext">Service</span>
+                    <div class="tooltip"><i><FontAwesomeIcon icon={faMagnifyingGlass} /></i>
+                      <span class="tooltiptext">Service</span>
                     </div>
                   </div>
                 </li>
 
                 <li>
                   <div className="tooltip-container">
-                    <div className="tooltip"><i><FontAwesomeIcon icon={faMagnifyingGlass} /></i>
-                      <span className="tooltiptext">Service</span>
+                    <div class="tooltip"><i><FontAwesomeIcon icon={faMagnifyingGlass} /></i>
+                      <span class="tooltiptext">Service</span>
                     </div>
                   </div>
                 </li>
