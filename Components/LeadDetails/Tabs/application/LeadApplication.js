e9ba8c1f1d6a60c125b80fdc38676cada1086328
ArunSpiderWorks <arun@spiderworks.in>
2024-05-29 12:25:54 +0530

    latest chnages

--- Components/LeadDetails/Tabs/application/LeadApplication.js
+++ Components/LeadDetails/Tabs/application/LeadApplication.js
@@ -255,7 +255,17 @@ function LeadApplication({ data, lead_id, handleLeadRefresh }) {
 
             <div className='lead-tabpanel-content-block timeline'>
                 <div className='lead-tabpanel-content-block-title'>
-                    <h2>Applications</h2>
+                    <div class="grid grid-cols-1 md:grid-cols-1 gap-4">
+
+                    <div className='lead-detail-title'>
+                    Edit and Preview Details of Students
+                      
+                    </div>
+
+
+
+                    </div>
+
                     <Grid display={'flex'} alignItems={'end'}>
                         {
                             !data?.user &&
@@ -282,11 +292,11 @@ function LeadApplication({ data, lead_id, handleLeadRefresh }) {
                         <Grid>
                             {
                                 data?.user ?
-                                    <Button variant='contained' onClick={handleCreate} className='bg-sky-500 mr-4' sx={{ color: 'white', '&:hover': { backgroundColor: '#0c8ac2' } }}>Apply</Button>
+                                    <Button variant='contained' onClick={handleCreate} className='edit-btn' sx={{ color: 'white', '&:hover': { backgroundColor: '#0c8ac2' } }}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M9 17H15M9 14H15M13.0004 3.00087C12.9048 3 12.7974 3 12.6747 3H8.2002C7.08009 3 6.51962 3 6.0918 3.21799C5.71547 3.40973 5.40973 3.71547 5.21799 4.0918C5 4.51962 5 5.08009 5 6.2002V17.8002C5 18.9203 5 19.4801 5.21799 19.9079C5.40973 20.2842 5.71547 20.5905 6.0918 20.7822C6.51921 21 7.079 21 8.19694 21L15.8031 21C16.921 21 17.48 21 17.9074 20.7822C18.2837 20.5905 18.5905 20.2842 18.7822 19.9079C19 19.4805 19 18.9215 19 17.8036V9.32568C19 9.20302 18.9999 9.09553 18.999 9M13.0004 3.00087C13.2858 3.00348 13.4657 3.01407 13.6382 3.05547C13.8423 3.10446 14.0379 3.18526 14.2168 3.29492C14.4186 3.41857 14.5918 3.59181 14.9375 3.9375L18.063 7.06298C18.4089 7.40889 18.5809 7.58136 18.7046 7.78319C18.8142 7.96214 18.8953 8.15726 18.9443 8.36133C18.9857 8.53379 18.9964 8.71454 18.999 9M13.0004 3.00087L13 5.80021C13 6.92031 13 7.48015 13.218 7.90797C13.4097 8.2843 13.7155 8.59048 14.0918 8.78223C14.5192 9 15.079 9 16.1969 9H18.999" stroke="#232648" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/></svg>Apply</Button>
                                     :
                                     <Tooltip title="Only for Applicants" >
                                         <a>
-                                            <Button variant='contained' disabled={true} className='bg-sky-500 mr-4' sx={{ color: 'white', '&:hover': { backgroundColor: '#0c8ac2' } }}>Apply</Button>
+                                            <Button variant='contained' disabled={true} className='edit-btn' sx={{ color: 'white', '&:hover': { backgroundColor: '#0c8ac2' } }}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M9 17H15M9 14H15M13.0004 3.00087C12.9048 3 12.7974 3 12.6747 3H8.2002C7.08009 3 6.51962 3 6.0918 3.21799C5.71547 3.40973 5.40973 3.71547 5.21799 4.0918C5 4.51962 5 5.08009 5 6.2002V17.8002C5 18.9203 5 19.4801 5.21799 19.9079C5.40973 20.2842 5.71547 20.5905 6.0918 20.7822C6.51921 21 7.079 21 8.19694 21L15.8031 21C16.921 21 17.48 21 17.9074 20.7822C18.2837 20.5905 18.5905 20.2842 18.7822 19.9079C19 19.4805 19 18.9215 19 17.8036V9.32568C19 9.20302 18.9999 9.09553 18.999 9M13.0004 3.00087C13.2858 3.00348 13.4657 3.01407 13.6382 3.05547C13.8423 3.10446 14.0379 3.18526 14.2168 3.29492C14.4186 3.41857 14.5918 3.59181 14.9375 3.9375L18.063 7.06298C18.4089 7.40889 18.5809 7.58136 18.7046 7.78319C18.8142 7.96214 18.8953 8.15726 18.9443 8.36133C18.9857 8.53379 18.9964 8.71454 18.999 9M13.0004 3.00087L13 5.80021C13 6.92031 13 7.48015 13.218 7.90797C13.4097 8.2843 13.7155 8.59048 14.0918 8.78223C14.5192 9 15.079 9 16.1969 9H18.999" stroke="#232648" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/></svg>Apply</Button>
                                         </a>
                                     </Tooltip>
                             }
@@ -308,7 +318,7 @@ function LeadApplication({ data, lead_id, handleLeadRefresh }) {
                                             <TableHead>
                                                 <TableRow>
                                                     <TableCell>
-                                                        <Typography variant="subtitle1" sx={{ color: 'black' }} fontWeight="bold">
+                                                        <Typography className='app-tab-title' variant="subtitle1" sx={{ color: 'black' }} fontWeight="bold">
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="18" viewBox="0 0 22 18" fill="none">
                                                                 <path d="M1 17H3M3 17H14M3 17V11.3682C3 10.8428 3 10.58 3.063 10.335C3.11883 10.1178 3.21073 9.91184 3.33496 9.72518C3.47505 9.51468 3.67114 9.33842 4.06152 8.9877L6.3631 6.91997C7.11784 6.24192 7.49549 5.90264 7.92249 5.77393C8.29894 5.66045 8.7007 5.66045 9.07715 5.77393C9.50451 5.90275 9.88267 6.2422 10.6387 6.92139L12.9387 8.9877C13.3295 9.33881 13.5245 9.51456 13.6647 9.72518C13.7889 9.91184 13.8807 10.1178 13.9365 10.335C13.9995 10.58 14 10.8428 14 11.3682V17M14 17H19M19 17H21M19 17V4.19691C19 3.07899 19 2.5192 18.7822 2.0918C18.5905 1.71547 18.2837 1.40973 17.9074 1.21799C17.4796 1 16.9203 1 15.8002 1H9.2002C8.08009 1 7.51962 1 7.0918 1.21799C6.71547 1.40973 6.40973 1.71547 6.21799 2.0918C6 2.51962 6 3.08009 6 4.2002V7.0002" stroke="#0B0D23" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                                                             </svg> University
@@ -317,14 +327,14 @@ function LeadApplication({ data, lead_id, handleLeadRefresh }) {
 
                                                     <TableCell>
 
-                                                        <Typography variant="subtitle1" sx={{ color: 'black' }} fontWeight="bold">
+                                                        <Typography className='app-tab-title' variant="subtitle1" sx={{ color: 'black' }} fontWeight="bold">
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="23" viewBox="0 0 22 23" fill="none">
                                                                 <path d="M2.75 5.74992H2.76759M2.76759 5.74992H19.2325M2.76759 5.74992C2.75 6.05119 2.75 6.42258 2.75 6.90011V16.1001C2.75 17.1735 2.75 17.7095 2.94982 18.1195C3.12559 18.4801 3.40585 18.7741 3.75081 18.9579C4.14261 19.1666 4.65575 19.1666 5.68053 19.1666L16.3195 19.1666C17.3443 19.1666 17.8567 19.1666 18.2484 18.9579C18.5934 18.7741 18.8746 18.4801 19.0504 18.1195C19.25 17.7099 19.25 17.1742 19.25 16.1028L19.25 6.89695C19.25 6.42095 19.25 6.05053 19.2325 5.74992M2.76759 5.74992C2.78954 5.37395 2.83889 5.10717 2.94982 4.87956C3.12559 4.51891 3.40585 4.22591 3.75081 4.04216C4.14299 3.83325 4.65675 3.83325 5.68351 3.83325H16.3168C17.3436 3.83325 17.8563 3.83325 18.2484 4.04216C18.5934 4.22591 18.8746 4.51891 19.0504 4.87956C19.1613 5.10717 19.2106 5.37395 19.2325 5.74992M19.2325 5.74992H19.25M13.75 10.5416L10.0833 14.3749L8.25 12.4583" stroke="#232648" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                                                             </svg>  Subject Area
                                                         </Typography>
                                                     </TableCell>
                                                     <TableCell>
-                                                        <Typography variant="subtitle1" sx={{ color: 'black' }} fontWeight="bold">
+                                                        <Typography className='app-tab-title' variant="subtitle1" sx={{ color: 'black' }} fontWeight="bold">
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                                                 <path d="M12 9.7998V19.9998M12 9.7998C12 8.11965 12 7.27992 12.327 6.63818C12.6146 6.0737 13.0732 5.6146 13.6377 5.32698C14.2794 5 15.1196 5 16.7998 5H19.3998C19.9599 5 20.2401 5 20.454 5.10899C20.6422 5.20487 20.7948 5.35774 20.8906 5.5459C20.9996 5.75981 21 6.04004 21 6.6001V15.4001C21 15.9601 20.9996 16.2398 20.8906 16.4537C20.7948 16.6419 20.6425 16.7952 20.4543 16.8911C20.2406 17 19.961 17 19.402 17H16.5693C15.6301 17 15.1597 17 14.7334 17.1295C14.356 17.2441 14.0057 17.4317 13.701 17.6821C13.3568 17.965 13.096 18.3557 12.575 19.1372L12 19.9998M12 9.7998C12 8.11965 11.9998 7.27992 11.6729 6.63818C11.3852 6.0737 10.9263 5.6146 10.3618 5.32698C9.72004 5 8.87977 5 7.19961 5H4.59961C4.03956 5 3.75981 5 3.5459 5.10899C3.35774 5.20487 3.20487 5.35774 3.10899 5.5459C3 5.75981 3 6.04004 3 6.6001V15.4001C3 15.9601 3 16.2398 3.10899 16.4537C3.20487 16.6419 3.35774 16.7952 3.5459 16.8911C3.7596 17 4.03901 17 4.59797 17H7.43073C8.36994 17 8.83942 17 9.26569 17.1295C9.64306 17.2441 9.99512 17.4317 10.2998 17.6821C10.6426 17.9638 10.9017 18.3526 11.4185 19.1277L12 19.9998" stroke="#0B0D23" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                                                             </svg>    Course
@@ -332,7 +342,7 @@ function LeadApplication({ data, lead_id, handleLeadRefresh }) {
                                                     </TableCell>
 
                                                     <TableCell>
-                                                        <Typography variant="subtitle1" sx={{ color: 'black' }} fontWeight="bold">
+                                                        <Typography className='app-tab-title' variant="subtitle1" sx={{ color: 'black' }} fontWeight="bold">
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="19" viewBox="0 0 18 19" fill="none">
                                                                 <path d="M1 6.66667H17M1 6.66667V14.978C1 16.0358 1 16.5645 1.21799 16.9686C1.40973 17.324 1.71547 17.6132 2.0918 17.7943C2.5192 18 3.07899 18 4.19691 18H13.8031C14.921 18 15.48 18 15.9074 17.7943C16.2837 17.6132 16.5905 17.324 16.7822 16.9686C17 16.5649 17 16.037 17 14.9812V6.66667M1 6.66667V5.9113C1 4.85342 1 4.32409 1.21799 3.92003C1.40973 3.56461 1.71547 3.27586 2.0918 3.09477C2.51962 2.88889 3.08009 2.88889 4.2002 2.88889H5M17 6.66667V5.90819C17 4.85238 17 4.32369 16.7822 3.92003C16.5905 3.56461 16.2837 3.27586 15.9074 3.09477C15.4796 2.88889 14.9203 2.88889 13.8002 2.88889H13M13 1V2.88889M13 2.88889H5M5 1V2.88889" stroke="#232648" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                                                             </svg>   Intake
@@ -341,7 +351,7 @@ function LeadApplication({ data, lead_id, handleLeadRefresh }) {
 
 
                                                     <TableCell>
-                                                        <Typography variant="subtitle1" sx={{ color: 'black' }} fontWeight="bold">
+                                                        <Typography className='app-tab-title' variant="subtitle1" sx={{ color: 'black' }} fontWeight="bold">
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="19" viewBox="0 0 18 19" fill="none">
                                                                 <path d="M5 6.00008V13.9044C5 15.0386 5 15.6056 5.1701 15.9526C5.48537 16.5959 6.17631 16.9656 6.88639 16.8711C7.2695 16.8201 7.74136 16.5055 8.68508 15.8764L8.68735 15.8749C9.0614 15.6255 9.24846 15.5008 9.44413 15.4316C9.80351 15.3046 10.1956 15.3046 10.555 15.4316C10.7511 15.5009 10.9389 15.6261 11.3144 15.8765C12.2582 16.5057 12.7305 16.82 13.1137 16.871C13.8237 16.9654 14.5146 16.5959 14.8299 15.9526C15 15.6056 15 15.0384 15 13.9044V5.99734C15 5.06575 15 4.59925 14.8185 4.24308C14.6587 3.92948 14.4031 3.6747 14.0895 3.51491C13.733 3.33325 13.2669 3.33325 12.3335 3.33325H7.66683C6.73341 3.33325 6.26635 3.33325 5.90983 3.51491C5.59623 3.6747 5.34144 3.92948 5.18166 4.24308C5 4.5996 5 5.06666 5 6.00008Z" stroke="#0B0D23" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                                                             </svg> Stage
@@ -349,16 +359,21 @@ function LeadApplication({ data, lead_id, handleLeadRefresh }) {
                                                     </TableCell>
 
                                                     <TableCell>
-                                                        <Typography variant="subtitle1" sx={{ color: 'black' }} fontWeight="bold">
+                                                        <Typography className='app-tab-title' variant="subtitle1" sx={{ color: 'black' }} fontWeight="bold">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+  <path d="M11.4385 7.89563C11.3318 7.59301 11.1337 7.33097 10.8717 7.14567C10.6097 6.96036 10.2967 6.86091 9.97574 6.86104H8.77492C8.43234 6.85924 8.10126 6.98454 7.8457 7.21271C7.59015 7.44087 7.42828 7.7557 7.39139 8.09629C7.3545 8.43689 7.44521 8.77907 7.64599 9.05666C7.84677 9.33425 8.14335 9.52753 8.47837 9.59912L10.3069 9.99807C10.6807 10.0799 11.0112 10.297 11.2347 10.6077C11.4582 10.9184 11.5591 11.3007 11.5178 11.6812C11.4766 12.0618 11.2962 12.4136 11.0113 12.6692C10.7264 12.9248 10.3571 13.0661 9.97441 13.0659H8.94115C8.2656 13.0659 7.69112 12.6351 7.47835 12.0327M9.45845 6.86104V5.30914M9.45845 14.6178V13.0673M2.99023 18.7881V15.4636H6.31477" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M18.1379 8.55654C18.4909 10.4385 18.2091 12.3844 17.3366 14.0889C16.4641 15.7933 15.0503 17.1598 13.3172 17.9739C11.584 18.788 9.62966 19.0035 7.76077 18.5867C5.89189 18.1699 4.21431 17.1444 2.99132 15.671M1.14819 11.7375C0.795161 9.85549 1.07702 7.90957 1.94952 6.20511C2.82203 4.50066 4.2358 3.13416 5.96892 2.32009C7.70205 1.50602 9.65643 1.29047 11.5253 1.70727C13.3942 2.12407 15.0718 3.14962 16.2948 4.62296" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M16.2952 1.50587V4.83041H12.9707" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
                                                             Uni. Deposit
                                                         </Typography>
                                                     </TableCell>
 
-                                                    <TableCell>
-                                                        <Typography sx={{ color: 'black' }} fontWeight="bold">
+                                                    {/* <TableCell>
+                                                        <Typography className='app-tab-title' sx={{ color: 'black' }} fontWeight="bold">
 
                                                         </Typography>
-                                                    </TableCell>
+                                                    </TableCell> */}
                                                 </TableRow>
                                             </TableHead>
                                             <TableBody>
@@ -408,6 +423,13 @@ function LeadApplication({ data, lead_id, handleLeadRefresh }) {
                                                                         // </svg> Add</Button>
                                                                     }
                                                                 </TableCell>
+                                                                
+                                                              
+
+                                                            </TableRow>
+
+                                                            <div className='application-table-inner-head'>
+                                                                <TableRow>
                                                                 <TableCell>
                                                                     {
                                                                         obj?.app_coordinator_status == null &&
@@ -472,8 +494,8 @@ function LeadApplication({ data, lead_id, handleLeadRefresh }) {
                                                                             <path d="M12 8L9.5 11L7 8" stroke="white" stroke-width="0.8" stroke-linecap="round" stroke-linejoin="round" /></svg></>
                                                                     )} */}
                                                                 </TableCell>
-
-                                                            </TableRow>
+                                                                </TableRow>
+                                                                </div>
                                                             {isRowExpanded(obj.id) && (
                                                                 // <TableRow>
                                                                 <TableCell colSpan={7} style={{ padding: 0, borderTop: 'none' }} >
