e50f6327a17e67e0ad3de79d6a9ac15ccc95b21d
ArunSpiderWorks <arun@spiderworks.in>
2024-05-30 12:36:35 +0530

    latest changes

--- Components/LeadDetails/Tabs/document/LeadDocuments.js
+++ Components/LeadDetails/Tabs/document/LeadDocuments.js
@@ -281,8 +281,11 @@ function LeadDocuments({ lead_id, from, app_id, app_details, appRefresh }) {
                                         {
                                             (!documentSelected?.file && documentSelected?.status == "Requested") &&
                                             <div className='doc-preview-block'>
+                                                <div className='doc-req-icon doc-request'>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="41" height="41" viewBox="0 0 41 41" fill="none"><path d="M20.5 11.9583V20.5H29.0417M20.5 35.875C12.0086 35.875 5.125 28.9914 5.125 20.5C5.125 12.0086 12.0086 5.125 20.5 5.125C28.9914 5.125 35.875 12.0086 35.875 20.5C35.875 28.9914 28.9914 35.875 20.5 35.875Z" stroke="#FEA878" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
+                                                </div>
                                                 <h2>This Document has been Requested</h2>
-                                                <span onClick={() => handleUploadRejectedDoc(documentSelected)} style={{ textDecoration: 'underLine' }}>Click here to Upload</span>
+                                                <span onClick={() => handleUploadRejectedDoc(documentSelected)} style={{ textDecoration: 'underLine' }}>Please upload if we received it and approve it.</span>
                                             </div>
                                         }
                                     </>
