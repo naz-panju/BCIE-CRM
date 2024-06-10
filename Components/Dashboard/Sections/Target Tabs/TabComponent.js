f0364aaed6f01b0385dbcb57d1b2ab8918eb7a91
muzammilSW <muzammil@spiderworks.in>
2024-06-10 17:31:33 +0530

    build error

--- Components/Dashboard/Sections/Target Tabs/TabComponent.js
+++ Components/Dashboard/Sections/Target Tabs/TabComponent.js
@@ -5,25 +5,14 @@ function ApplicationSubmittedGraph({ data }) {
     return (
         <div >
             <div className='flex items-center justify-between' style={{ height: '180px' }}>
-<<<<<<< HEAD
                 <div className='target-inner-item'>
                     <div className='target-inner-item-title target'>Targets</div>
-                    <h3> 40</h3>
+                    <h3> {data?.target || 0}</h3>
                 </div>
                 <DoughnutChartComponent />
                 <div className='target-inner-item'>
                     <div className='target-inner-item-title acheived'>Acheived</div>
-                    <h3>55</h3>
-=======
-                <div>
-                    Targets
-                    <div> {data?.target || 0}</div>
-                </div>
-                <DoughnutChartComponent data={data} />
-                <div>
-                    Achievd
-                    <div> {data?.achievement || 0}</div>
->>>>>>> 11346ce4f5b68d6b8790ec2fc3667fa32f65c800
+                    <h3>{data?.achievement || 0}</h3>
                 </div>
             </div>
         </div>
