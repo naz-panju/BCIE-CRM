709cb0e3b1513b45a1e091b8bff4d5388908565d
muzammilSW <muzammil@spiderworks.in>
2024-06-12 19:18:14 +0530

    buld error

--- pages/goals-and-targets.js
+++ pages/goals-and-targets.js
@@ -3,6 +3,7 @@ import GoalsIndex from '@/Components/Goals-and-Targets/GoalsIndex'
 import React from 'react'
 
 function GoalsAndTargets() {
+    // test
     return (
         <Layout>
             <GoalsIndex />
