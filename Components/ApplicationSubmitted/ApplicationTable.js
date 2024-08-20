08fb6d718628244261c6a40f3aaf12eb1b8107a6
muzammil@spiderworks.in <muzammil@spiderworks.in>
2024-08-20 20:37:07 +0530

    app table

--- Components/ApplicationSubmitted/ApplicationTable.js
+++ Components/ApplicationSubmitted/ApplicationTable.js
@@ -1301,7 +1301,7 @@ export default function ApplicationSubmittedTable({ refresh, editId, setEditId, 
                                                                 </TableCell>
                                                                 <TableCell align="left"> {row?.course_level?.name}</TableCell>
                                                                 <TableCell align="left"> {row?.course}</TableCell>
-                                                                <TableCell align="left"> {row?.subject_area?.name}</TableCell>
+                                                                {/* <TableCell align="left"> {row?.subject_area?.name}</TableCell> */}
                                                                 <TableCell><Tooltip title={row?.differ_intake_note}>{row?.intake?.name}</Tooltip></TableCell>
                                                                 <TableCell className='stage-colm' align="left"><Tooltip title={row?.stage_note}><span style={{ backgroundColor: row?.stage?.colour }} className='stage-span'>{row?.stage?.name}</span></Tooltip></TableCell>
                                                                 <TableCell align="left">
