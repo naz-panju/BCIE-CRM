6f8903c93afc3ed820006c3b6037eb89259974c8
muzammil@spiderworks.in <muzammil@spiderworks.in>
2024-08-27 18:44:20 +0530

    dashboard

--- Components/Dashboard/Charts/BarColorGraph.js
+++ Components/Dashboard/Charts/BarColorGraph.js
@@ -6,25 +6,22 @@ const BarColorChartComponent = ({ leadStage }) => {
 
   useEffect(() => {
     const ctx = chartRef.current.getContext('2d');
-    // const colors = ['#e73f76', '#322fc8', '#ff9900', '#66b3ff', '#99ff99', '#ffcc99', '#c2c2f0', '#ff0000', '#00ff00', '#ffff00']; // Example color palette (10 colors)
 
     const labels = leadStage?.data?.map(item => item.name);
     const data = leadStage?.data?.map(item => item.lead_count);
-    const colors = leadStage?.data?.map(item => item.colour);
-
-    // console.log(labels, data, colors);
+    const colors = leadStage?.data?.map(item => item.lead_count > 0 ? item.colour : 'rgba(0, 0, 0, 0)');
 
     const chart = new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: labels, // 10 labels
+        labels: labels,
         datasets: [
           {
             label: '',
-            data: data, // 10 data points
-            backgroundColor: colors, // Use the first 10 colors from the palette
-            borderWidth: 0, // Remove border
-            barThickness: 16, // Adjust this value for narrower bars
+            data: data,
+            backgroundColor: colors,
+            borderWidth: 0,
+            barThickness: 16,
           },
         ],
       },
@@ -32,31 +29,30 @@ const BarColorChartComponent = ({ leadStage }) => {
         responsive: true,
         plugins: {
           legend: {
-            display: false, // Hide legend by default
+            display: false,
           },
           title: {
-            display: false, // Hide title by default
+            display: false,
           },
         },
         scales: {
           y: {
             beginAtZero: true,
-            // Adjust the minimum value to add space between bars and top of the chart
-            suggestedMin: 0, // This can be adjusted if you want more padding
-            suggestedMax: Math.max(...data) * 1.1, // Optional: increases the max value to ensure space at the top
+            min: 0, // Ensure bars start at 0
+            grace: '5%', // Adds padding at the top of the chart
             grid: {
-                display: false,
+              display: false,
             },
             ticks: {
-                display: false,
+              display: false,
             },
-        },
+          },
           x: {
             grid: {
-              display: false, // Hide grid lines
+              display: false,
             },
             ticks: {
-              display: false, // Hide x-axis tick marks and labels
+              display: false,
             },
           },
         },
